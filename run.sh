70e1d54b2022fd54928986c01d445dd124e01ab2
seungkee <123use321@gmail.com>
2021-11-27 18:18:29 +0900

    2

---
+++ run.sh
@@ -0,0 +1,9 @@
+find . -type f -iname "*.sh" -exec chmod +x {} \;
+cd /submission/code
+./install.sh
+cd /submission
+./makesoftlink.sh
+cd /submission/code/descriptor_track/train_eval/
+./descriptor_run.sh
+cd /submission/code/matching_track/train_eval/
+./matching_run.sh
